657d2104a1a1fbb1844985c21e3be01ff5a64638
Emi <emineme02@gmail.com>
2022-12-10 23:24:37 -0300

    Fix undo and redo functions for each Type of Operation

--- src/backend/OperationType.java
+++ src/backend/OperationType.java
@@ -1,94 +1,109 @@
 package backend;
 
 import backend.model.Figure;
+import javafx.scene.paint.Color;
 
 public enum OperationType {
-    DRAW("Dibujar una figura") {
+    DRAW("Dibujar un ") {
         public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.deleteFigure(oldFigure);
+            canvasState.deleteFigure(newFigure);
         }
         public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            canvasState.addFigure(newFigure);
+        }
+    },
+    DELETE("Borrar un ") {
+        public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState){
             canvasState.addFigure(oldFigure);
         }
-    },
-    DELETE("Borrar una figura") {
-        public void undo(Figure oldFigure,Figure newFigure, CanvasState canvasState){
-            canvasState.addFigure(oldFigure);
-        }
 
         @Override
-        public void redo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
+        public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
             canvasState.deleteFigure(oldFigure);
         }
     },
-    CHANGECOLOR("Cambiar el color de borde") {
-        @Override
-        public void undo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setLineColor(oldFigure.getLineColor());
-        }
-
-        @Override
-        public void redo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setLineColor(oldFigure.getLineColor());
-        }
-    },
-    CHANGEFILL("Cambiar el color de relleno") {
-        @Override
-        public void undo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setFillColor(oldFigure.getFillColor());
-        }
-
-        @Override
-        public void redo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setFillColor(oldFigure.getFillColor());
-        }
-
-    },
-    CHANGEBORDER("Cambiar el formato") {
-        @Override
-        public void undo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setBorderSize(oldFigure.getBorderSize());
-        }
-        public void redo(Figure oldFigure,Figure newFigure, CanvasState canvasState) {
-            newFigure.setBorderSize(oldFigure.getBorderSize());
-        }
-    },
-    COPYFORMAT("Copiar el formato") {
+    CHANGECOLOR("Cambiar color del borde de ") {
         @Override
         public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.setAuxFigure(newFigure);
+            Color aux = newFigure.getLineColor();
+            newFigure.setLineColor(oldFigure.getLineColor());
+            oldFigure.setLineColor(aux);
         }
 
         @Override
         public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-             canvasState.setAuxFigure(oldFigure);
+            Color aux = newFigure.getLineColor();
+            newFigure.setLineColor(oldFigure.getLineColor());
+            oldFigure.setLineColor(aux);
         }
     },
-    COPYFIGURE("Copiar una figura") {
+    CHANGEFILL("Cambiar color de relleno de ") {
         @Override
         public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.setAuxFigure(newFigure);
+            Color aux = newFigure.getFillColor();
+            newFigure.setFillColor(oldFigure.getFillColor());
+            oldFigure.setFillColor(aux);
         }
 
         @Override
         public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            Color aux = newFigure.getFillColor();
+            newFigure.setFillColor(oldFigure.getFillColor());
+            oldFigure.setFillColor(aux);
+        }
+
+    },
+    CHANGEBORDER("Cambiar tamaño del borde de ") {
+        @Override
+        public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            double aux = newFigure.getBorderSize();
+            newFigure.setBorderSize(oldFigure.getBorderSize());
+            oldFigure.setBorderSize(aux);
+        }
+        public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            Color aux = newFigure.getFillColor();
+            newFigure.setFillColor(oldFigure.getFillColor());
+            oldFigure.setFillColor(aux);
+        }
+    },
+    COPYFORMAT("Copiar formato de ") {
+        @Override
+        public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            canvasState.setAuxFigure(newFigure.clone());
+            newFigure.setFormat(oldFigure);
+        }
+
+        @Override
+        public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            newFigure.setFormat(canvasState.getAuxFigure());
+            canvasState.setAuxFigure(null);
+        }
+    },
+    COPYFIGURE("Copiar un ") {
+        @Override
+        public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
             canvasState.setAuxFigure(oldFigure);
         }
+
+        @Override
+        public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
+            canvasState.setAuxFigure(newFigure);
+        }
     },
-    CUTFIGURE("Cortar una figura") {
+    CUTFIGURE("Cortar un ") {
         @Override
         public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.setAuxFigure(newFigure);
-            canvasState.addFigure(oldFigure);
+            canvasState.setAuxFigure(oldFigure);
+            canvasState.addFigure(newFigure);
         }
 
         @Override
         public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.setAuxFigure(oldFigure);
+            canvasState.setAuxFigure(newFigure);
             canvasState.deleteFigure(oldFigure);
         }
     },
-    PASTEFIGURE("Pegar una figura") {
+    PASTEFIGURE("Pegar un ") {
         @Override
         public void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
             canvasState.deleteFigure(newFigure);
@@ -97,19 +112,20 @@ public enum OperationType {
 
         @Override
         public void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState) {
-            canvasState.addFigure(oldFigure);
-            canvasState.setAuxFigure(newFigure);
+            canvasState.addFigure(newFigure);
+            canvasState.setAuxFigure(null);
         }
     };
 
     private final String name;
-    public abstract void undo(Figure oldFigure,Figure newFigure, CanvasState canvasState);
-    public abstract void redo(Figure oldFigure,Figure newFigure, CanvasState canvasState);
+    public abstract void undo(Figure oldFigure, Figure newFigure, CanvasState canvasState);
+    public abstract void redo(Figure oldFigure, Figure newFigure, CanvasState canvasState);
     OperationType(String name) {
         this.name = name;
     }
 
-    public String getName() {
+    @Override
+    public String toString() {
         return name;
     }
 }
